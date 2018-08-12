66871c2c0517e68359bdb6636b9c615b9d557c9f
John Murphy <murf2073@gmail.com>
2018-08-11 20:25:07 -0400

    fixed loop

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 
 client.on('message', (msg) => {
-	msg.reply('pong');
+	if(msg == 'ping'){
+		msg.reply('pong');
+	}	
 });
 
 client.login('NDc3OTkxNjkwNzkyMzM3NDA4.DlEMKg.BrKk6QL0QBkvZLYI1MOVOQu26_0');
