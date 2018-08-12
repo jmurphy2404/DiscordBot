08481580aa3ce9fcd249fbfe8020ec8f36b08c1c
John Murphy <murf2073@gmail.com>
2018-08-11 20:17:32 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+
+client.on('message', (msg) => {
+	msg.reply('pong');
+});
+
+client.login('NDc3OTkxNjkwNzkyMzM3NDA4.DlEMKg.BrKk6QL0QBkvZLYI1MOVOQu26_0');
