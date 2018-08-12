99620ebf9d38c824fd1a1156c5f6a4beb35c1d39
John Murphy <murf2073@gmail.com>
2018-08-11 20:52:44 -0400

    regex to setup message recognition for reminders:

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 
 client.on('message', (msg) => {
-	if(msg == 'ping'){
-		msg.reply('pong');
-	}	
+	// if(msg == 'test'){
+	// 	msg.reply('pong');
+	const re = /^(?:#remind)/;
+	let convMsg = msg.toString();
+	let bool = re.test(convMsg);
+	if (bool === true) {
+		msg.reply('confirmed');
+	}
 });
 
 client.login('NDc3OTkxNjkwNzkyMzM3NDA4.DlEMKg.BrKk6QL0QBkvZLYI1MOVOQu26_0');
